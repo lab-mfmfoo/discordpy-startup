2a24227b978ba2dbfc2dec0659dfcd8a5e7adda8
lab-mfmfoo <62243615+lab-mfmfoo@users.noreply.github.com>
2020-07-10 17:15:37 +0900

    debug

--- discordbot.py
+++ discordbot.py
@@ -402,7 +402,7 @@ async def on_message(message):
     if command == "!test":
         for root,dirs,files in os.walk(os.getcwd()):
 	    for file in files:
-	        path = os.path.join(root,file)
+		path = os.path.join(root,file)
 		print(path)
         await message.channel.send(randint(1,6))
         with open("test/お題.csv") as f:
